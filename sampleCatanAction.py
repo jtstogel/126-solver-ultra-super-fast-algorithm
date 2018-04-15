3acd79cfe1ada7284dcb59313c3177c870fd83a3
Lindsay Yang <Lindsay@Lindsays-MacBook-Air.local>
2018-04-14 21:17:32 -0700

    plan board

--- sampleCatanAction.py
+++ sampleCatanAction.py
@@ -10,12 +10,26 @@ def action(self):
         rmax, rmin = np.argmax(self.resources), np.argmin(self.resources)
         self.trade(rmax,rmin)
     return
-
+'''
 def planBoard(baseBoard):
     x = genRand(0,baseBoard.width+1)
     y = genRand(0,baseBoard.height+1)
     optSettlementLoc = (x,y)
     return optSettlementLoc
+'''
+def planBoard(baseBoard):
+    print ("plan board")
+    resources_per_tile = {}
+    for v in range(baseBoard.max_vertex):
+        loc = baseBoard.get_vertex_location(v)
+        resources_per_tile[loc] = (set(), 0)
+        for i in [-1, 0]:
+            for j in [-1,0]:
+                neighbor = (loc[0]+i, loc[1] + j)
+                if neighbor.is_tile():
+                    resources_per_tile[loc][0].add(baseBoard.resources[neighbor])
+                    resources_per_tile[loc][1] += 1
+    optimal_loc = max(resources_per_tile.keys(), key = lambda x: len(resources_per_tile[x][0]) + resources_per_tile[x][1])
 
 
 def genRand(low,high):
