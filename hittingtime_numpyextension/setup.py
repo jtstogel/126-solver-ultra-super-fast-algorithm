f4ecb0e59ca6d72ab429ab3b2daf7bf2d4306da9
Jackson Stogel <jtstogel@berkeley.edu>
2018-04-17 16:36:09 -0700

    added optimize flag and compiler selection

--- hittingtime_numpyextension/setup.py
+++ hittingtime_numpyextension/setup.py
@@ -17,6 +17,7 @@ def configuration(parent_package='', top_path=None):
                              top_path)
       config.add_extension('fast_transition_matrix',
                            ['transition_matrix.c'],
+                           extra_compile_args=['-std=c99', '-O2'],
                            extra_info=info)
 
       return config
